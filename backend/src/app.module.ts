f0e0ec2bccb2d1818cd4b8ebc999fdac3f78800b
Vadim Bondarev <koshey88@MacBook-Air-Vadim.local>
2026-05-16 22:04:03 +0300

    fix: remove redis dependency to debug startup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { StaffModule }        from './staff/staff.module';
 import { ServicesModule }     from './services/services.module';
@@ -11,7 +10,6 @@ import { PrismaModule }       from './prisma/prisma.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
-    ThrottlerModule.forRoot([{ ttl: 60_000, limit: 100 }]),
     PrismaModule,
     AppointmentsModule,
     StaffModule,
