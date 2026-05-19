0f3935670d3120316e739b0f975fbb428123a6b5
Vadim Bondarev <koshey88@MacBook-Air-Vadim.local>
2026-05-19 09:34:58 +0300

    fix: restore all modules to app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { StaffModule }        from './staff/staff.module';
+import { ServicesModule }     from './services/services.module';
+import { ClientsModule }      from './clients/clients.module';
+import { NotificationsModule }from './notifications/notifications.module';
+import { PrismaModule }       from './prisma/prisma.module';
 
 @Module({
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    PrismaModule,
+    AppointmentsModule,
+    StaffModule,
+    ServicesModule,
+    ClientsModule,
+    NotificationsModule,
+  ],
 })
 export class AppModule {}
